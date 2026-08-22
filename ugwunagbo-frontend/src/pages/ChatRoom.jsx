69e2445f9c69324a830d1ea7dc471be27c13871a
Zealot <officialzealot418@gmail.com>
2026-08-22 20:10:59 +0100

    Restrict dashboard access to administrators

--- ugwunagbo-frontend/src/pages/ChatRoom.jsx
+++ ugwunagbo-frontend/src/pages/ChatRoom.jsx
@@ -1283,7 +1283,9 @@ const ChatRoom = () => {
               Live Chat
             </h3>
 
-            
+            <p className="text-[10px] text-white/80 sm:text-xs">
+              Room for everyone!
+            </p>
           </div>
         </div>
 
